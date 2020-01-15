20ac848eae796b532963c8e2a1d24bc8481bde68
aprestoncafe <32875301+aprestoncafe@users.noreply.github.com>
2020-01-15 11:31:04 -0500

    Finish out ability scores

--- js/javascript.js
+++ js/javascript.js
@@ -27,3 +27,88 @@ $(".strength")
 		}); 
 	})
 	.keyup();
+	$(".constitution")
+	.keyup(function() {
+		var sum = 0;
+		$(".constitution").each(function() {
+			var val = $.trim( $(this).val() );
+
+			if ( val ) {
+				val = parseFloat( val.replace( /^\$/, "" ) );
+				sum += !isNaN( val ) ? val : 0;
+			}
+			$(".conTotal").text(sum);
+			
+			var modifier = Math.floor(((sum - 10) / 2))
+			$(".conModifier").text(modifier);
+		}); 
+	})
+	.keyup();
+	$(".dexterity")
+	.keyup(function() {
+		var sum = 0;
+		$(".dexterity").each(function() {
+			var val = $.trim( $(this).val() );
+
+			if ( val ) {
+				val = parseFloat( val.replace( /^\$/, "" ) );
+				sum += !isNaN( val ) ? val : 0;
+			}
+			$(".dexTotal").text(sum);
+			
+			var modifier = Math.floor(((sum - 10) / 2))
+			$(".dexModifier").text(modifier);
+		}); 
+	})
+	.keyup();
+	$(".intelligence")
+	.keyup(function() {
+		var sum = 0;
+		$(".intelligence").each(function() {
+			var val = $.trim( $(this).val() );
+
+			if ( val ) {
+				val = parseFloat( val.replace( /^\$/, "" ) );
+				sum += !isNaN( val ) ? val : 0;
+			}
+			$(".intTotal").text(sum);
+			
+			var modifier = Math.floor(((sum - 10) / 2))
+			$(".intModifier").text(modifier);
+		}); 
+	})
+	.keyup();
+	$(".wisdom")
+	.keyup(function() {
+		var sum = 0;
+		$(".wisdom").each(function() {
+			var val = $.trim( $(this).val() );
+
+			if ( val ) {
+				val = parseFloat( val.replace( /^\$/, "" ) );
+				sum += !isNaN( val ) ? val : 0;
+			}
+			$(".wisTotal").text(sum);
+			
+			var modifier = Math.floor(((sum - 10) / 2))
+			$(".wisModifier").text(modifier);
+		}); 
+	})
+	.keyup();
+	$(".charisma")
+	.keyup(function() {
+		var sum = 0;
+		$(".charisma").each(function() {
+			var val = $.trim( $(this).val() );
+
+			if ( val ) {
+				val = parseFloat( val.replace( /^\$/, "" ) );
+				sum += !isNaN( val ) ? val : 0;
+			}
+			$(".chaTotal").text(sum);
+			
+			var modifier = Math.floor(((sum - 10) / 2))
+			$(".chaModifier").text(modifier);
+		}); 
+	})
+	.keyup();
